5a9b8bfd58fdbb988cddfd7d72c7ce77be63454b
“justinLiang1994” <273080406@qq.com>
2023-04-27 11:40:42 +0800

    test1 update2

--- test1/src/test1.java
+++ test1/src/test1.java
@@ -12,6 +12,6 @@ public class test1
 
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("hello world2");
+        System.out.println("hello ok");
     }
 }
